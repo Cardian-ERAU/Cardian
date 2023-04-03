a8d4b1a4b643f6a87399cfb121c1f12d3515e57f
awfaith48 <afaith48@gmail.com>
2023-04-03 16:28:41 -0400

    making scrollbars and txtlist formtting

--- src/main/java/com/cardian/CheckboxController.java
+++ src/main/java/com/cardian/CheckboxController.java
@@ -9,17 +9,21 @@ import java.util.List;
 import javafx.fxml.FXML;
 import javafx.scene.control.CheckBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.control.ScrollPane;
 
 public class CheckboxController {
-    
-    @FXML 
+
+    @FXML
     public VBox checkboxContainer;
 
-    @FXML 
+    @FXML
     public void initialize() {
-        List<String> data = loadDataFromFile("assets\\checklist.txt"); // list.txt being the file of every thing of the checkbox
+        List<String> data = loadDataFromFile("assets\\checklist.txt"); // list.txt being the file of every thing of the
+                                                                       // checkbox
+        ScrollPane scrollPane = new ScrollPane(); // create a scrollpane
+        scrollPane.setContent(checkboxContainer); // set content of scrollpane
+        scrollPane.setPrefSize(400, 400); // sizing of the scrollpane
 
-        // this is a comment to show how to commit
         for (String str : data) {
             CheckBox checkboxx = new CheckBox(str);
             checkboxx.setSelected(false);
@@ -35,8 +39,7 @@ public class CheckboxController {
                 data.add(line);
                 line = br.readLine();
             }
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return data;
