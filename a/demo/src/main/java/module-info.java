b343a3e18228867606cdd59a5cb52d713085e3ae
Gabriel Lindo <lindog@my.erau.edu>
2023-04-05 16:21:05 -0400

    Amended to run on my device without warnings

--- a/demo/src/main/java/module-info.java
+++ a/demo/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.cardian {
     requires javafx.controls;
     requires javafx.fxml;
+    requires transitive javafx.graphics;
 
     opens com.cardian to javafx.fxml;
     exports com.cardian;
